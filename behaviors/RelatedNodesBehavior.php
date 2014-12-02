b7974b27d0657c686fe169c8bf3b332ee10f9bfc
Fredrik Wollsén <fredrik@neam.se>
2014-12-02 15:00:58 +0100

    Added helper methods for specifying relational graph db relations

--- behaviors/RelatedNodesBehavior.php
+++ behaviors/RelatedNodesBehavior.php
@@ -41,6 +41,41 @@ class RelatedNodesBehavior extends CActiveRecordBehavior
 
     }
 
+    public function relationalGraphDbRelation($relationName, $modelClass)
+    {
+        return array(
+            $relationName => array(
+                self::HAS_MANY,
+                $modelClass,
+                array('id' => 'node_id'),
+                'through' => 'outNodes',
+                'condition' => 'relation = :relation',
+                'order' => 'outEdges.weight ASC',
+                'params' => array(
+                    ':relation' => $relationName,
+                ),
+            )
+        );
+    }
+
+    public function relationalGraphDbRelatedOutNodes()
+    {
+        $relationName = 'related';
+        return array(
+            $relationName => array(
+                CActiveRecord::HAS_MANY,
+                'Node',
+                array('id' => 'id'),
+                'through' => 'outNodes',
+                'condition' => 'relation=:relation',
+                'order' => 'outEdges.weight ASC',
+                'params' => array(
+                    ':relation' => $relationName
+                ),
+            )
+        );
+    }
+
     /**
      * Handles node id arrays sent in post data with the relation name as array key. Example:
      *
