c04fbe3161d88385880ffda5b04d99983e258101
Fredrik Wollsén <fredrik@neam.se>
2014-12-02 10:28:49 +0100

    Moved in and cleaned up some workflow ui controller logic into related nodes behavior where it belongs

--- behaviors/RelatedNodesBehavior.php
+++ behaviors/RelatedNodesBehavior.php
@@ -42,6 +42,149 @@ class RelatedNodesBehavior extends CActiveRecordBehavior
     }
 
     /**
+     * Handles node id arrays sent in post data with the relation name as array key. Example:
+     *
+     * array(3) {
+     *   ["foo"]=>
+     *   string(0) ""
+     *   ["bar"]=>
+     *   string(4) "barz"
+     *   ["related"]=>
+     *   array(3) {
+     *     [0]=>
+     *     string(1) "1"
+     *     [1]=>
+     *     string(1) "3"
+     *     [2]=>
+     *     string(1) "5"
+     *   }
+     * }
+     *
+     * @param $post array
+     */
+    public function handlePostedEdges($post)
+    {
+
+        $model = $this->owner;
+        $node = $model->node();
+
+        // array of models relation-names
+        $relationNames = array_keys($model->relations());
+
+        // Relations considered safe. Note: set the relation as safe in the model-rules (if it is not already)!
+        $allowedRelations = array_filter(
+        // attribute-names posted from form
+            array_keys($post),
+            // add to list if the attribute is a safe relation
+            function ($attribute) use ($model, $relationNames) {
+                return in_array($attribute, $relationNames) && $model->isAttributeSafe($attribute);
+            }
+        );
+
+        foreach ($allowedRelations as $relationName) {
+            $this->setOutEdges($post[$relationName], $relationName);
+        }
+    }
+
+
+    /**
+     * Sets the out-edges of the current model
+     *
+     * @param $relationName
+     * @param array $futureOutEdgesNodeIds
+     */
+    public function setOutEdges(array $futureOutEdgesNodeIds, $relationName)
+    {
+
+        $model = $this->owner;
+        $node = $model->node();
+
+        $isFutureOutEdgesEmpty = empty($futureOutEdgesNodeIds);
+
+        // Delete all outEdges for the relation if none is present in
+        // form and the model has some outEdges (ie user removed edges)
+        if ($isFutureOutEdgesEmpty && count($model->{$relationName}) > 0) {
+            Edge::model()->deleteAllByAttributes(array(
+                'from_node_id' => $node->id,
+                'relation' => $relationName,
+            ));
+        }
+
+        if ($isFutureOutEdgesEmpty) {
+            $futureOutEdgesNodeIds = array();
+        }
+
+        $this->deleteEdgeDiff($futureOutEdgesNodeIds, $relationName);
+
+        // TODO: fetch weights properly when they are implemented in form
+
+        $weight = 0;
+        foreach ($futureOutEdgesNodeIds as $toNodeId) {
+            $this->addEdge($node->id, $toNodeId, $relationName, $weight++);
+        }
+
+    }
+
+    /**
+     * Deletes the edges which are present but not in future-edges
+     *
+     * @param $model ActiveRecord
+     * @param $futureEdges array
+     * @param $relationName string
+     * @return int number of edges deleted
+     */
+    protected function deleteEdgeDiff(array $futureOutEdgesNodeIds, $relationName)
+    {
+        $model = $this->owner;
+
+        // {1,2,3}
+        $currentOutEdges = $model->getRelatedModelColumnValues($relationName, 'id');
+
+        // {1,2,3} complement {2,3,4} = {1}
+        $edgesToDelete = array_diff($currentOutEdges, $futureOutEdgesNodeIds);
+
+        $criteria = new CDbCriteria();
+        $criteria->addCondition('from_node_id = :from');
+        $criteria->addCondition('relation = :relation');
+        $criteria->addInCondition('to_node_id', $edgesToDelete);
+        $criteria->params[':from'] = $model->node()->id;
+        $criteria->params[':relation'] = $relationName;
+
+        return Edge::model()->deleteAll($criteria);
+    }
+
+    protected function addEdge($fromNodeId, $toNodeId, $relation, $weight = null)
+    {
+        $edge = Edge::model()->findByAttributes(array(
+            'from_node_id' => $fromNodeId,
+            'to_node_id' => $toNodeId,
+            'relation' => $relation,
+        ));
+
+        // Nothing has changed
+        if ($edge !== null && $weight === null) {
+            return;
+        }
+
+        if ($edge === null) {
+            $edge = new Edge();
+        }
+
+        $edge->from_node_id = $fromNodeId;
+        $edge->to_node_id = $toNodeId;
+        $edge->relation = $relation;
+
+        if ($weight !== null) {
+            $edge->weight = $weight;
+        }
+
+        if (!$edge->save()) {
+            throw new SaveException($edge);
+        }
+        return true;
+    }
+
+    /**
      * @param string $relation name of the relation
      * @param string $idColumn the column which values are collected from the related items
      * @return array
