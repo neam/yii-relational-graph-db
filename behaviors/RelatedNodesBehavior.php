f950d482feb734772221ced93c7616cb449bcb62
Fredrik Wollsén <fredrik@neam.se>
2014-12-02 15:01:29 +0100

    Refactored a variable name

--- behaviors/RelatedNodesBehavior.php
+++ behaviors/RelatedNodesBehavior.php
@@ -185,12 +185,12 @@ class RelatedNodesBehavior extends CActiveRecordBehavior
         return Edge::model()->deleteAll($criteria);
     }
 
-    protected function addEdge($fromNodeId, $toNodeId, $relation, $weight = null)
+    protected function addEdge($fromNodeId, $toNodeId, $relationName, $weight = null)
     {
         $edge = Edge::model()->findByAttributes(array(
             'from_node_id' => $fromNodeId,
             'to_node_id' => $toNodeId,
-            'relation' => $relation,
+            'relation' => $relationName,
         ));
 
         // Nothing has changed
@@ -204,7 +204,7 @@ class RelatedNodesBehavior extends CActiveRecordBehavior
 
         $edge->from_node_id = $fromNodeId;
         $edge->to_node_id = $toNodeId;
-        $edge->relation = $relation;
+        $edge->relation = $relationName;
 
         if ($weight !== null) {
             $edge->weight = $weight;
@@ -217,14 +217,14 @@ class RelatedNodesBehavior extends CActiveRecordBehavior
     }
 
     /**
-     * @param string $relation name of the relation
+     * @param string $relationName name of the relation
      * @param string $idColumn the column which values are collected from the related items
      * @return array
      */
-    public function getRelatedModelColumnValues($relation, $idColumn)
+    public function getRelatedModelColumnValues($relationName, $idColumn)
     {
         $ids = array();
-        foreach ($this->owner->{$relation} as $related) {
+        foreach ($this->owner->{$relationName} as $related) {
             $ids[] = $related->{$idColumn};
         }
         return $ids;
