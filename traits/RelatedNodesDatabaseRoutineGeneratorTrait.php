cdd9593a636bed551f8202bf87b6dfb2cd55146d
Fredrik Wollsén <fredrik@neam.se>
2015-03-16 13:02:08 +0200

    Added RelatedNodesDatabaseRoutineGeneratorTrait skeleton

---
+++ traits/RelatedNodesDatabaseRoutineGeneratorTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Trait RelatedNodesDatabaseRoutineGeneratorTrait
+ *
+ * @license BSD-3-Clause
+ * @author See https://github.com/neam/yii-relational-graph-db/graphs/contributors
+ */
+trait RelatedNodesDatabaseRoutineGeneratorTrait
+{
+
+    public function relatedNodesRoutines($dryRun, $echo)
+    {
+
+        $echo("foo");
+
+    }
+
+}
