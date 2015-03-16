d29ef8092462b90003dc54e69d53c3ae9aa60ea2
Fredrik Wollsén <fredrik@neam.se>
2015-03-16 13:37:58 +0200

    Generator trait to create triggers that removes orphaned node after removing the original item

--- traits/RelatedNodesDatabaseRoutineGeneratorTrait.php
+++ traits/RelatedNodesDatabaseRoutineGeneratorTrait.php
@@ -12,7 +12,42 @@ trait RelatedNodesDatabaseRoutineGeneratorTrait
     public function relatedNodesRoutines($dryRun, $echo)
     {
 
-        $echo("foo");
+        $itemTypes = ItemTypes::where('is_graph_relatable');
+
+        $this->d("Connecting to '" . $this->_db->connectionString . "'\n");
+
+        foreach ($itemTypes as $modelClass => $table) {
+            if ($this->_checkTableAndColumnExists($table, "node_id")) {
+
+                $routineName = "remove_node_when_removing_$table";
+
+                $sql = "";
+                $sql .= "       AFTER DELETE ON `$table`\n";
+                $sql .= "       FOR EACH ROW\n";
+                $sql .= "       DELETE FROM node WHERE id = old.node_id;\n";
+                $sql .= "       END\n";
+
+                $dropRoutineSql = "DROP TRIGGER IF EXISTS $routineName";
+
+                $createRoutineSql = "CREATE TRIGGER $routineName $sql";
+
+                if ($this->_verbose) {
+                    $echo("\n");
+                    $echo($dropRoutineSql);
+                    $echo($createRoutineSql);
+                    $echo("\n");
+                }
+
+                if (!$dryRun) {
+                    $this->_db->createCommand($dropRoutineSql)->execute();
+                    //$this->_db->createCommand("DELIMITER $$")->execute();
+                    $this->_db->createCommand($createRoutineSql)->execute();
+                    //$this->_db->createCommand("DELIMITER ;")->execute();
+                }
+
+            }
+
+        }
 
     }
 
